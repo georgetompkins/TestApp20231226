92aebbe5515b7b5fe008176244cff99b59d373c3
George Tompkins <39093787+georgetompkins@users.noreply.github.com>
2023-12-27 01:28:25 -0600

    Initial Commit

---
+++ TestApp20231226/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestApp20231226
+//
+//  Created by George Tompkins on 12/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestApp20231226/TestApp20231226App.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp20231226App.swift
+//  TestApp20231226
+//
+//  Created by George Tompkins on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp20231226App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

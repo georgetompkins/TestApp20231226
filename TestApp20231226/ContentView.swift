6ab717604b59e593fbea4f53dbab2235a97c445f
George Tompkins <39093787+georgetompkins@users.noreply.github.com>
2023-12-27 02:59:58 -0600

    basic temperature conversion

--- TestApp20231226/ContentView.swift
+++ TestApp20231226/ContentView.swift
@@ -8,16 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var startTemp = 0.0
+    let temperatureTypes = ["Celsius", "Fahrenheit", "Kelvin"]
+    @State private var fromType = "Celsius"
+    @State private var toType = "Kelvin"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Convert From") {
+                    TextField("input", value: $startTemp, format: .number)
+                        .font(.largeTitle)
+                    Picker("From", selection: $fromType) {
+                        ForEach(temperatureTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Convert To") {
+                    Picker("To", selection: $toType) {
+                        ForEach(temperatureTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    Text("\(convertTemp(startTemp: startTemp, convertFrom: fromType, convertTo: toType), specifier: "%.2f")°")
+                        .font(.largeTitle)
+                }
+            }
+            .navigationTitle("Temperature Conversion")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
+
+func convertTemp(startTemp: Double, convertFrom: String, convertTo: String) -> Double {
+    var result: Double = 0
+    if convertFrom == "Celsius" {
+        switch convertTo {
+        case "Celsius":
+            result = startTemp
+        case "Fahrenheit":
+            result = (startTemp * 9/5) + 32
+        case "Kelvin":
+            result = startTemp + 273.15
+        default:
+            result = 0
+        }
+    } else if convertFrom == "Fahrenheit" {
+        switch convertTo {
+        case "Celsius":
+            result = (startTemp - 32) * 5/9
+        case "Fahrenheit":
+            result = startTemp
+        case "Kelvin":
+            result = (startTemp - 32) * 5/9 + 273.15
+        default:
+            result = 0
+        }
+    } else if convertFrom == "Kelvin" {
+        switch convertTo {
+        case "Celsius":
+            result = startTemp - 273.15
+        case "Fahrenheit":
+            result = (startTemp - 273.15) * 9/5 + 32
+        case "Kelvin":
+            result = startTemp
+        default:
+            result = 0
+        }
+    } else {
+        result = 0
+    }
+    return result
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
